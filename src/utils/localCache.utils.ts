bb36e61fe19cdd18f4234b431d84fb3061cff5bd
Paolo Micheletti <p.micheletti@hastega.it>
2022-08-08 14:28:06 +0200

    started refactoring of foldercache system

--- src/utils/localCache.utils.ts
+++ src/utils/localCache.utils.ts
@@ -1,8 +1,10 @@
 import { existsSync, promises, writeFileSync } from 'fs'
 
-export const readWriteRequest = (folderPath: string, fileName: string, data: JSON) => {
+export const readWriteRequest = async (folderPath: string, fileName: string, data: JSON) => {
     if (existsSync(folderPath)) {
-        promises.readFile(folderPath+fileName)
+        await promises.readFile(folderPath+'/'+fileName+'.json').then((res) => {
+            data = JSON.parse(res.toString())
+        })
     } else {
         promises.mkdir(folderPath, { recursive: true }).then(() =>
             writeFileSync(folderPath + '/' + fileName + '.json', JSON.stringify(data))
